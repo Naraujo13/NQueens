b0e083c9f83085665c2bb676b1a9270f46cdd5fe
Nícolas Oreques de Araujo <nicolas.de.araujo@hotmail.com>
2017-08-14 00:55:35 -0300

    Corrigido erro na passagem dos vetores

--- vectorVersion.cpp
+++ vectorVersion.cpp
@@ -7,7 +7,7 @@
 #include <thread>
 #include <future>
 
-#define N 5
+#define N 6
 //Solution
 int solution[N];
 std::mutex solutionMutex;
@@ -19,6 +19,13 @@ bool verifySolution(int copy[N-1], int threadId){
     for (int i = 0; i<N-1; i++)
         test[i+1] = copy[i];
 
+//    for (int i = 0; i<N; i++){
+//        for (int j = 0; j<N; j++){
+//            if (i != j && test[i] == test[j])
+//                return false;
+//        }
+//    }
+
     for (int i = 0; i < N; i++){
         for (int j = i + 1; j < N; j++){
             //Diagonal Inferior Direita
@@ -29,12 +36,14 @@ bool verifySolution(int copy[N-1], int threadId){
                 return false;
         };
     };
+
     return true;
 }
 
 
 void printBoard(int solution[N])
 {
+    std::fflush(stdout);
     for (int i = 0; i < N; i++) {
 
         //Divisor above lines
@@ -59,6 +68,7 @@ void printBoard(int solution[N])
         std::cout << "+---";
     }
     std::cout<<"+" << std::endl;
+    std::fflush(stdout);
 }
 
 void setSolution(int copy[N-1], int threadId){
@@ -81,11 +91,11 @@ void setSolution(int copy[N-1], int threadId){
     solutionMutex.unlock();
 }
 
-void permutate(int original[N], std::promise<bool>&& p, std::shared_future<bool> f[N], int threadId){
+void permutate(int aux[N-1], std::promise<bool>&& p, std::shared_future<bool> f[N], int threadId){
 
     int copy[N-1];
     for (int i = 0; i < N-1; i ++)
-        copy[i] = original[i+1];
+        copy[i] = aux[i];
 
     while(std::next_permutation(copy, copy + N-1)){
         //Verifica se outra thread achou a solução
@@ -108,11 +118,11 @@ void permutate(int original[N], std::promise<bool>&& p, std::shared_future<bool>
 
 int main(){
 
-
-
     //Preenche vetor
+    solutionMutex.lock();
     for (int i = 0; i < N; i++)
         solution[i] = i;
+    solutionMutex.unlock();
 
     std::promise<bool> p[N];
     std::shared_future<bool> f[N];
@@ -124,20 +134,31 @@ int main(){
     for (int i=0; i<N; i++){
         int aux[N-1];
 
-
-        for (int j = 0, k = 0; k<N-1, j<N; j++, k++){
-            if (i != j)
+        //Cria array de permutação inicial dessa thread para dividir o trabalho
+        for (int j = 0, k = 0; k<N-1, j<N; j++){
+            if (i != j) {
                 aux[k] = j;
+                k++;
+            }
         }
 
+        //Cria thread
         t[i] = std::thread(permutate, std::ref(aux), std::move(p[i]), std::ref(f), i); //change solution each iteration
-        //t[i].detach();
+
     }
 
+    //Join nas threads
     for (int i = 0 ; i < N; i++)
         t[i].join();
 
+    //Printa solução
+    solutionMutex.lock();
+    std::cout <<std::endl<<std::endl<< "Solução final:" <<std::endl;
+    for (int i =0; i<N; i++)
+        std::cout << solution[i];
+    std::cout<<std::endl;
     printBoard(solution);
+    solutionMutex.unlock();
 
     return 0;
 }
